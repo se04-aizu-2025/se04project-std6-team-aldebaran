4e9f9734dbbdd981f1c6c3f2b63e853ec2b3c327
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2025-12-12 16:33:16 +0900

    Initial project setup.

---
+++ StellaSort/src/main/java/com/aldebaran/stellasort/Launcher.java
@@ -0,0 +1,9 @@
+package com.aldebaran.stellasort;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
