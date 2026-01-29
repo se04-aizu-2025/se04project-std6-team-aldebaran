c38808c743a48046818ced02fe54f01968cb988c
garcar <kiran.einheuser@gmail.com>
2026-01-30 07:50:07 +0800

    Hooked up buttons to the controller and checks for valid array input

--- StellaSort/src/main/java/com/aldebaran/stellasort/controller/SortTabController.java
+++ StellaSort/src/main/java/com/aldebaran/stellasort/controller/SortTabController.java
@@ -1,12 +1,21 @@
 package com.aldebaran.stellasort.controller;
 import com.aldebaran.stellasort.service.SortAlgorithm;
 import javafx.fxml.FXML;
-
+import javafx.scene.control.TextArea;
+import javafx.scene.chart.BarChart;
+import java.util.Arrays;
+import javafx.scene.control.Label;
 
 public class SortTabController {
 
     private SortAlgorithm algorithm;
 
+	@FXML private TextArea inputArea;
+    @FXML private BarChart<String, Number> arrayBarChartNode;
+	@FXML private Label statusLabel;
+	
+	private int[] array;
+	
     public void setAlgorithm(SortAlgorithm algorithm) {
         this.algorithm = algorithm;
     }
@@ -20,4 +29,34 @@ public class SortTabController {
             case QUICK -> {}
         }
     }
+	
+	@FXML
+	private void onRandomize() {
+	
+	}
+	
+	@FXML
+	private void onInputArray() {
+    try {
+        String text = inputArea.getText();
+        array = Arrays.stream(text.split("[,\\s]+"))
+                      .mapToInt(Integer::parseInt)
+                      .toArray();
+					  
+		statusLabel.setText("");
+
+    } catch (Exception e) {
+        statusLabel.setText("Invalid input");
+    }
 }
+	
+	
+}
+
+
+
+
+
+
+
+	
