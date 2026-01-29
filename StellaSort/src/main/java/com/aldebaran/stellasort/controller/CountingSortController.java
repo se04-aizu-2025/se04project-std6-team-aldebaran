d4a76126f585b2ed15a7ce17649e10cd2c416d9d
CaptainSpaceCadet <s1302005@u-aizu.ac.jp>
2026-01-29 20:21:09 +0900

    Integrate `InputArray` and `ArrayDisplay` into `CountingSortController`
    - Add `InputArray` and `ArrayDisplay` components to manage and visualize input arrays.
    - Update methods to utilize `InputArray` for input validation and processing.
    - Enhance array visualization using `ArrayDisplay` with CSS styling.
    - Adjust bar chart CSS class name to `bar-highlighted`.

--- StellaSort/src/main/java/com/aldebaran/stellasort/controller/CountingSortController.java
+++ StellaSort/src/main/java/com/aldebaran/stellasort/controller/CountingSortController.java
@@ -2,13 +2,16 @@ package com.aldebaran.stellasort.controller;
 
 import com.aldebaran.stellasort.animation.ArrayBarChartAnimator;
 import com.aldebaran.stellasort.component.ArrayBarChart;
-import com.aldebaran.stellasort.service.CountingSort;
-import javafx.animation.Animation;
-import javafx.animation.SequentialTransition;
+import com.aldebaran.stellasort.component.ArrayDisplay;
+import com.aldebaran.stellasort.component.InputArray;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO: figure out how best to load the array into the bar chart
@@ -21,6 +24,19 @@ public class CountingSortController {
     /*
      * The text field displays the input array
      */
+    @FXML private TextArea inputArrayTextArea;
+    @FXML private Label inputArrayErrorMessageLabel;
+    @FXML private Button inputArrayButton; // disabled until input is valid
+
+    private InputArray inputArray;
+    private final List<Integer> input = new ArrayList<>();
+
+    /*
+     * The input and result array display
+     */
+
+    @FXML private FlowPane inputArrayFlowPane;
+    private ArrayDisplay inputArrayDisplay;
 
     /*
      * The play button starts the sorting process
@@ -40,6 +56,8 @@ public class CountingSortController {
     private ArrayBarChartAnimator arrayBarChartAnimator;
 
     @FXML private void initialize() {
+        inputArray = new InputArray(inputArrayTextArea, inputArrayErrorMessageLabel, inputArrayButton, (i) -> (i < 0) ? "All input values must be positive integers" : null);
+        inputArrayDisplay = new ArrayDisplay(inputArrayFlowPane);
         arrayBarChart = new ArrayBarChart(barChart);
         arrayBarChartAnimator = new ArrayBarChartAnimator(arrayBarChart);
         arrayBarChart.initializeBarChart();
@@ -51,32 +69,39 @@ public class CountingSortController {
 
     @FXML private void onPlaySortButtonClick() {
         List<Integer> inputArray = List.of(2, 1, 3, 5, 3, 2);
-        arrayBarChart.setBarChart(inputArray, (int i) -> (i % 2 == 0) ? "bar-highlight" : null );
+        arrayBarChart.setBarChart(inputArray, (int i) -> (i % 2 == 0) ? "bar-highlighted" : null );
+
+        inputArrayDisplay.setArray(inputArray, (i) -> (i % 2 == 0) ? "array-element-highlighted" : null);
     }
 
     @FXML private void onSetInputButtonClick() {
-        List<Integer> inputArray = List.of(3, 3, 3, 1, 1, 6, 6, 7, 8, 9);
-        int max = inputArray.stream().max(Integer::compareTo).orElse(0);
-        List<Integer> zeroArray = Collections.nCopies(inputArray.size(), 0);
-
-        arrayBarChart.setYBounds(0, inputArray.size()+1);
-        arrayBarChart.setBarChart(zeroArray, ArrayBarChart.NO_RULE);
-
-        List<Animation> countingSortAnimations = CountingSort.getSortAnimations(
-                inputArray,
-                max,
-                (index, oldValue) -> {
-                    return List.of(arrayBarChartAnimator.incrementBarAnimation(index, oldValue));
-                },
-                (index, oldValue) -> {
-                    return List.of(arrayBarChartAnimator.decrementBarAnimation(index, oldValue));
-                }
-        );
-
-        SequentialTransition sequence = new SequentialTransition();
-        sequence.getChildren().addAll(countingSortAnimations);
-        sequence.play();
+//        List<Integer> inputArray = List.of(3, 3, 3, 1, 1, 6, 6, 7, 8, 9);
+//        int max = inputArray.stream().max(Integer::compareTo).orElse(0);
+//        List<Integer> zeroArray = Collections.nCopies(inputArray.size(), 0);
+//
+//        arrayBarChart.setYBounds(0, inputArray.size()+1);
+//        arrayBarChart.setBarChart(zeroArray, ArrayBarChart.NO_RULE);
+//
+//        List<Animation> countingSortAnimations = CountingSort.getSortAnimations(
+//                inputArray,
+//                max,
+//                (index, oldValue) -> {
+//                    return List.of(arrayBarChartAnimator.incrementBarAnimation(index, oldValue));
+//                },
+//                (index, oldValue) -> {
+//                    return List.of(arrayBarChartAnimator.decrementBarAnimation(index, oldValue));
+//                }
+//        );
+//
+//        SequentialTransition sequence = new SequentialTransition();
+//        sequence.getChildren().addAll(countingSortAnimations);
+//        sequence.play();
     }
 
-    //#region BarChart Methodd
+    @FXML private void onInputArrayButtonClicked() {
+        inputArray.onInputButtonClick(input);
+        inputArrayDisplay.setArray(input, ArrayDisplay.NO_RULE);
+    }
+
+    //#region BarChart Method
 }
